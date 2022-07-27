abbef86fbf25c75361c40d2cf49f63c7cc4d2d0b
mfd2007 <jens.cordt@bsi.bund.de>
2022-07-27 10:19:15 +0200

    Add downloader.

---
+++ downloader/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE_NAME=csaf_downloader
+
+echo "Build $IMAGE_NAME image"
+
+docker build -t $IMAGE_NAME:latest .
+
+VERSION=$(docker run --rm --entrypoint cat $IMAGE_NAME:latest /version)
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION 
---
+++ downloader/csaf_downloader.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm --env OPTIONS="$@" csaf_downloader:latest
