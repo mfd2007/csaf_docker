feaa6d59a81f3aa0e427ff53589d658a13b0ae6c
mfd2007 <jens.cordt@bsi.bund.de>
2022-07-27 08:00:40 +0200

    Created image to compile all binaries.

---
+++ base/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE_NAME=csaf_base
+
+echo "Build CSAF base image & compile binaries"
+
+docker build -t $IMAGE_NAME:latest .
+
+VERSION=$(docker run --rm --entrypoint cat $IMAGE_NAME:latest /version)
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION 
