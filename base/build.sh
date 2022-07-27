1a850be29f303cf2525c2a6fe7b3bee514cec7e6
mfd2007 <jens.cordt@bsi.bund.de>
2022-07-27 08:14:14 +0200

    Removed caching from base image build

--- base/build.sh
+++ base/build.sh
@@ -4,7 +4,7 @@ IMAGE_NAME=csaf_base
 
 echo "Build CSAF base image & compile binaries"
 
-docker build -t $IMAGE_NAME:latest .
+docker build --no-cache -t $IMAGE_NAME:latest .
 
 VERSION=$(docker run --rm --entrypoint cat $IMAGE_NAME:latest /version)
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION 
