65de4c3aae48cc58ac7eae3542799eb589c941f1
mfd2007 <jens.cordt@bsi.bund.de>
2022-08-08 09:48:48 +0200

    Improvement on aggregator

--- aggregator/root/docker-entrypoint.sh
+++ aggregator/root/docker-entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+mkdir /data-local/
+cp -r /data/* /data-local/
+
 if [ $INTERIM == "1" ]
 then 
 	/usr/local/csaf_aggregator --config=/config/csaf/aggregator.toml --interim >> /data/log/interim.log
 else
 	/usr/local/csaf_aggregator --config=/config/csaf/aggregator.toml >> /data/log/aggregate.log
 fi
+
+rm -rf /data/archive/
+cp -r /data-local/* /data/
+chown user:user -R /data/*
+rm -rf /data-local/
 
 sleep $DELAY
