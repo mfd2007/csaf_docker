4cce9995827a85b2295615cd6ce0c58c63ccf728
mfd2007 <58845044+mfd2007@users.noreply.github.com>
2023-06-22 15:22:31 +0200

    fix: Remove intermediate docker images

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -4,10 +4,10 @@ IMAGE_NAME=csaf_base
 
 if [[ $# -eq 0 ]]; then
 	echo "Build CSAF base image & compile binaries"
-	docker build --no-cache -t $IMAGE_NAME:latest . 
+	docker build --no-cache --rm -t $IMAGE_NAME:latest . 
 else
 	echo "Build CSAF base image & compile binaries from branch $1"
-	docker build --no-cache --build-arg TAG=$1 -t $IMAGE_NAME:latest --network=host .
+	docker build --no-cache --rm --build-arg TAG=$1 -t $IMAGE_NAME:latest --network=host .
 fi
 
 VERSION=$(docker run --rm --entrypoint cat $IMAGE_NAME:latest /version)
--- docker/provider/build.sh
+++ docker/provider/build.sh
@@ -4,7 +4,7 @@ IMAGE_NAME=csaf_provider
 
 echo "Build $IMAGE_NAME image"
 
-docker build -t $IMAGE_NAME:latest .
+docker build --rm -t $IMAGE_NAME:latest .
 
 VERSION=$(docker run --rm --entrypoint cat $IMAGE_NAME:latest /version)
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION 
