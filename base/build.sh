b12fbc9e23441aad63c952e2da55ae2a3ad85441
mfd2007 <58845044+mfd2007@users.noreply.github.com>
2023-06-22 09:29:03 +0200

    feat: build specific branch

--- base/build.sh
+++ base/build.sh
@@ -2,9 +2,13 @@
 
 IMAGE_NAME=csaf_base
 
-echo "Build CSAF base image & compile binaries"
-
-docker build --no-cache -t $IMAGE_NAME:latest .
+if [[ $# -eq 0 ]]; then
+	echo "Build CSAF base image & compile binaries"
+	docker build --no-cache -t $IMAGE_NAME:latest . 
+else
+	echo "Build CSAF base image & compile binaries from branch $1"
+	docker build --no-cache --build-arg TAG=$1 -t $IMAGE_NAME:latest --network=host .
+fi
 
 VERSION=$(docker run --rm --entrypoint cat $IMAGE_NAME:latest /version)
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION 
